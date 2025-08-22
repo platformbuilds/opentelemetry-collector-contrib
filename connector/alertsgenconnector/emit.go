baefd1e765f77ddede039b4d7c547180ebfb0d8b
aarvee11 <rv@platformbuilds.org>
2025-08-23 01:40:27 +0530

    [feature] alertsgenconnector - first PR

--- connector/alertsgenconnector/emit.go
+++ connector/alertsgenconnector/emit.go
@@ -1,7 +1,6 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-
 package alertsgenconnector // import "github.com/open-telemetry/opentelemetry-collector-contrib/connector/alertsgenconnector"
 
 import (
